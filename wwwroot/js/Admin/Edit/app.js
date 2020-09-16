3f63900e24c47b609c705a83fc953fbcc29b647b
Sh1nku <42642351+Sh1nku@users.noreply.github.com>
2020-09-16 23:55:11 +0200

    Adding editor

--- wwwroot/js/Admin/Edit/app.js
+++ wwwroot/js/Admin/Edit/app.js
@@ -8,13 +8,25 @@
             },
             postVersion() {
                 return this.$store.state.postVersion;
+            },
+            parsedText() {
+                return this.$store.state.postVersion.rawText;
+            },
+            tab() {
+                return this.$store.state.tab;
             }
         },
         methods: {
+            setTab: function (value) {
+                this.$store.commit('setTab', value);
+            },
         },
         mounted: function () {
             this.$nextTick(function () {
                 this.$store.dispatch('init');
+                document.querySelectorAll('code').forEach((block) => {
+                    hljs.highlightBlock(block)
+                })
             });
         }
     });
--- wwwroot/js/Admin/Edit/store.js
+++ wwwroot/js/Admin/Edit/store.js
@@ -2,12 +2,16 @@ var store = new Vuex.Store({
     strict: true,
     state: {
         post: null,
-        postVersion: null
+        postVersion: null,
+        tab: 'edit'
     },
     mutations: {
         setModel: (state, value) => {
             state.post = value.post;
             state.postVersion = value.postVersion;
+        },
+        setTab: (state, value) => {
+            state.tab = value;
         }
     },
     actions: {
