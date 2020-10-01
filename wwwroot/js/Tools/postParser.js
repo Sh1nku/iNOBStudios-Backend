20d84659be9b93e762ab38aee9662cab3e3fc527
Sh1nku <42642351+Sh1nku@users.noreply.github.com>
2020-10-01 21:50:49 +0200

    Adding start of parsing of post text from input into preview

---
+++ wwwroot/js/Tools/postParser.js
@@ -0,0 +1,25 @@
+﻿function parsePost(text) {
+    let variables = [...text.matchAll(/\@\[(.{0,}?)\]\@/g)];
+    let resultText = [];
+    let i = 0;
+    let index = 0;
+    while (i != text.length) {
+        if (index != variables.length) {
+            if (variables[index].index == i) {
+                resultText.push(variables[index][0]);
+                i += variables[index][0].length;
+                index++;
+            }
+            else {
+                resultText.push(text.substr(i, variables[index].index).replace('\n','<br>'));
+                i = variables[index].index;
+            }
+        }
+        else {
+            resultText.push(text.substr(i).replace('\n', '<br>'));
+            i = text.length;
+        }
+    }
+
+    return resultText.join('');
+}
